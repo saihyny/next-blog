51555daaa569cfcaea299271bfdb4846d163970c
saihyny <satyasaikiranrocks@gmail.com>
2024-11-01 00:48:52 +0530

    commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
+   
     images: {
         domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com'],
     },
